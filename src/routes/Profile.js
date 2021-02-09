3eac8908179d62c38d520b833d9736ceae7228a2
unknown <luck2901@naver.com>
2021-02-09 16:01:39 +0900

    Update

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,5 +1,12 @@
 import React from "react";
+import { authService } from 'fbase';
+import { useHistory } from "react-router-dom";
 
-const Profile = () =><span>Profile</span>
-
+const Profile = () =>{
+    const onLogOutClick = () => authService.signOut();
+    return(
+    <>
+        <button onClick = {onLogOutClick}>Log Out</button>
+    </>)
+}
 export default Profile;
