0597b88bb3cb9ee2052f764ccd0b8181d608a672
unknown <luck2901@naver.com>
2021-02-10 16:53:14 +0900

    Update

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,6 +5,7 @@ import Tweet from './../components/Tweet';
 const Home = ({userObj}) =>{
     const [tweet, setTweet] = useState("");
     const [tweets, setTweets] = useState([]);
+    const [attachment, setAttachment] = useState() 
 
     useEffect(() =>{
         dbService.collection("tweets").onSnapshot((snapshot) =>{
@@ -28,13 +29,25 @@ const Home = ({userObj}) =>{
     const onFileChange = (event) =>{
          const{target:{files}} = event;
          const theFile = files[0];
+         const reader = new FileReader();
+         reader.onloadend = (finishedEvent) => {
+            const {currentTarget:{result}} = finishedEvent;
+            setAttachment(result);
+         }
+         reader.readAsDataURL(theFile);
     }
+    const onClearAttachmentClick = () => setAttachment(null);
     return(
     <div>
         <form onSubmit ={onSubmit}>
             <input value = {tweet} onChange= {onChange}type="text" placeholder="What is on your mind?" maxLength={120} />
             <input type = "file" accept="image/*" onChange={onFileChange}/> 
             <input type="submit" value="Tweet" />
+            {attachment && 
+            <div>
+                <img src={attachment} width="50px" height="50px" />
+                <button onClick={onClearAttachmentClick}>Clear</button>
+            </div>}
         </form>
         <div>
             {tweets.map((tweet) => 
