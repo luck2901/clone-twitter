2dcdfdf80d5e2b084278fc11e4a96afe4c7c4688
unknown <luck2901@naver.com>
2021-02-10 16:34:40 +0900

    Update

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -25,10 +25,15 @@ const Home = ({userObj}) =>{
         const{target:{value}}  = event;
         setTweet(value);
     };
+    const onFileChange = (event) =>{
+         const{target:{files}} = event;
+         const theFile = files[0];
+    }
     return(
     <div>
         <form onSubmit ={onSubmit}>
             <input value = {tweet} onChange= {onChange}type="text" placeholder="What is on your mind?" maxLength={120} />
+            <input type = "file" accept="image/*" onChange={onFileChange}/> 
             <input type="submit" value="Tweet" />
         </form>
         <div>
