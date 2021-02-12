34b45e2e6c99423e3d3c08251b2e9e98520560e4
luck2901 <luck2901@gmail.com>
2021-02-12 18:03:02 +0900

    Update

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,12 +2,12 @@ import React from "react";
 import {Link} from "react-router-dom";
 import Profile from './../routes/Profile';
 
-const Navigation = () =>
+const Navigation = ({userObj}) =>
 (
 <nav>
     <ul>
         <li><Link to="/">Home</Link></li>
-        <li><Link to="/profile">Profile</Link></li>
+        <li><Link to="/profile">{userObj.displayName}'s Profile</Link></li>
     </ul>
 </nav>
 );
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,23 +1,43 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { authService, dbService } from 'fbase';
 import { useHistory } from "react-router-dom";
 
 const Profile = ({userObj}) =>{
     const history = useHistory();
+    const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
     const onLogOutClick = () => {
         authService.signOut();
         history.push("/")
     };
     const getMyTweet = async() =>{
-        const tweets = await dbService.collection("tweets").where("creatorID","==",userObj.uid).orderBy("creatorAt").get(); //필터링하는 방법
-        //
+        const tweets = await dbService.collection("tweets").where("creatorID","==",userObj.uid).orderBy("creatorAt").get(); 
+        //필터링하는 방법
+        //orderby를 만드려면 index 생성해야함.
         tweets.docs.map(doc => doc.data());
+    }
+    const onChange = (e) =>{
+        const{
+            target:{value}
+        } = e;
+        setNewDisplayName(value);
+    }
+    const onSubmit = async(e) =>{
+        e.preventDefault();
+        if(userObj.displayName !==newDisplayName){
+            await userObj.updateProfile({
+                displayName: newDisplayName,
+            })
+        }
     }
     useEffect(() =>{
         getMyTweet();
     },[])
     return(
     <>
+    <form onSubmit = {onSubmit}>
+        <input onChange = {onChange} type="text" placeholder="Display name" value = {newDisplayName}/>
+        <input type="submit" value="Update Profile" />
+    </form>
         <button onClick = {onLogOutClick}>Log Out</button>
     </>)
 }
