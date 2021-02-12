72ece7eb9eca5facd8012b4af8f25de1a0d7b37a
luck2901 <luck2901@gmail.com>
2021-02-12 17:38:02 +0900

    Update

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,13 +1,21 @@
-import React from "react";
-import { authService } from 'fbase';
+import React, { useEffect } from "react";
+import { authService, dbService } from 'fbase';
 import { useHistory } from "react-router-dom";
 
-const Profile = () =>{
+const Profile = ({userObj}) =>{
     const history = useHistory();
     const onLogOutClick = () => {
         authService.signOut();
         history.push("/")
+    };
+    const getMyTweet = async() =>{
+        const tweets = await dbService.collection("tweets").where("creatorID","==",userObj.uid).orderBy("creatorAt").get(); //필터링하는 방법
+        //
+        tweets.docs.map(doc => doc.data());
     }
+    useEffect(() =>{
+        getMyTweet();
+    },[])
     return(
     <>
         <button onClick = {onLogOutClick}>Log Out</button>
